2029288aa5d8879a5d90664f9c6ead21dacce8fa
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2024-01-15 13:11:24 +0000

    Deploying to gh-pages from @ YuliaLevechko/EDNA@f3ea6c8101597ac7f7b17662e4964f9beaaf56cd 🚀

---
+++ index.57f34880.js
@@ -0,0 +1,2 @@
+new Swiper(".feedback-swiper-container",{pagination:{el:".feedback-swiper-pagination",type:"bullets",clickable:!0,dynamicByllets:!0},simulateTouch:!0,touchRatio:1,touchAngle:45,slideToClickedSlide:!0});
+//# sourceMappingURL=index.57f34880.js.map
